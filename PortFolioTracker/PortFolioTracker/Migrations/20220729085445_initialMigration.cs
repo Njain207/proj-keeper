bf9528bb1cc18470686fb709115b35ed48b23ecc
neha <jneha207@gmail.com>
2022-07-29 14:54:41 +0530

    PortFolioTracker:Login Database connectivity established

---
+++ PortFolioTracker/PortFolioTracker/Migrations/20220729085445_initialMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortFolioTracker.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Logins",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FIRSTNAME = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MIDDLENAME = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LASTNAME = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    USERNAME = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PASSWORD = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    STATUS = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logins", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logins");
+        }
+    }
+}
